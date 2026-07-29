4baad689b91a516e2c5aa52c0dea1da23f196348
thheffner10 <thheffner10@gmail.com>
2026-07-29 09:44:12 -0400

    Create passport-config.js
    passport set-up for OAuth

---
+++ backend/passport-config.js
@@ -0,0 +1,25 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "/auth/google/callback"
+},
+  function (accessToken, refreshToken, profile, done) {
+    // Here, you would typically find or create a user in your database
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
